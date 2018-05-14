94fad9bbad6164cd9f081149eadb67ad621f9cad
silentbessh <silentbessh@gmail.com>
2018-05-14 13:06:50 +0200

    dbloan
    dbloan

--- BodyLabVendingMachine/src/Database/DBLoan.java
+++ BodyLabVendingMachine/src/Database/DBLoan.java
@@ -1,5 +1,22 @@
 package Database;
 
-public class DBLoan {
+import java.util.List;
+
+import Infrastructure.DBLoanIF;
+import Model.Loan;
+
+public class DBLoan implements DBLoanIF {
+
+	@Override
+	public List<Loan> findLoanForCustomer(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int insertLoan(Loan l) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
