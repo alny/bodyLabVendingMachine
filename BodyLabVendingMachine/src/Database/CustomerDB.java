9affc2362c2d86ab927c5c6ae655031a41859258
drixmix <foxyhorse@gmail.com>
2018-05-14 10:33:19 +0200

    interface

---
+++ BodyLabVendingMachine/src/Database/CustomerDB.java
@@ -0,0 +1,24 @@
+package Database;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import Infrastructure.CustomerIF;
+import Model.Customer;
+
+public class CustomerDB implements CustomerIF {
+
+	@Override
+	public List<Customer> findAllCustomers() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Customer findCustomer(int customerId) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
--- BodyLabVendingMachine/src/Infrastructure/CustomerIF.java
+++ BodyLabVendingMachine/src/Infrastructure/CustomerIF.java
@@ -1,5 +1,14 @@
 package Infrastructure;
 
-public interface CustomerDBIF {
+import java.sql.SQLException;
+import java.util.List;
 
+import Model.Customer;
+
+public interface CustomerIF {
+	
+	List<Customer> findAllCustomers() throws SQLException;
+
+	Customer findCustomer(int customerId) throws SQLException;
+	
 }
