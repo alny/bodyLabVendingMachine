1ff8c0441427b3cf2051505b29fa9aa2d074f053
drixmix <foxyhorse@gmail.com>
2018-05-16 13:43:11 +0200

    Fixed

--- BodyLabVendingMachine/src/Infrastructure/DBVendingMachineIF.java
+++ BodyLabVendingMachine/src/Infrastructure/DBVendingMachineIF.java
@@ -1,9 +1,12 @@
 package Infrastructure;
 
+import Database.PersistensException;
 import Model.Product;
 import Model.VendingMachine;
 
 public interface DBVendingMachineIF {
-	VendingMachine findVendingMachine(int id);
-	int insertVendingMachine(VendingMachine vm);
+	
+	VendingMachine findVendingMachine(int id) throws PersistensException;
+
+	int insertVendingMachine(VendingMachine vm) throws PersistensException;
 }
