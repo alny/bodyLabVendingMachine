f03f6e5072f4ab01f1cd9d6210198bb9ad3c7906
silentbessh <silentbessh@gmail.com>
2018-05-31 10:11:10 +0200

    small fix

--- BodyLabVendingMachine/src/infrastructure/DBConnectionIF.java
+++ BodyLabVendingMachine/src/infrastructure/DBConnectionIF.java
@@ -6,12 +6,4 @@ import java.sql.SQLException;
 
 public interface DBConnectionIF {
 	public Boolean recheckConnection();
-	public void startTransaction() throws SQLException;
-	public void commitTransaction() throws SQLException;
-	public void rollbackTransaction() throws SQLException;
-	public int executeInsertWithIdentity(PreparedStatement ps) throws SQLException;
-	public int executeInsertWithIdentity(String sql) throws SQLException;
-	public int executeUpdate(String sql) throws SQLException;
-	public void disconnect();
-	public Connection getConnection();
 }
